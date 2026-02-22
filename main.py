2d71c6602d7b1f130051b358c88ad42a217fd6bd
64kun <havchyc@gmail.com>
2026-02-22 10:35:06 +0300

    add 'pages' parameter for -r flag

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import os
 cmd_parser = argparse.ArgumentParser()
 
 cmd_parser.add_argument('path', help="path to v3rmillion folder")
-cmd_parser.add_argument('-r', required=True, help="threads search", nargs="+")
-cmd_parser.add_argument('-pages', type=int, default=1)
+cmd_parser.add_argument('-r', help="threads search", nargs="+")
 
 cmd_args = cmd_parser.parse_args()
 
@@ -25,18 +24,29 @@ def tonumber(v, default:int=None, Not:bool=False):
 FILE_PATH = cmd_args.path
 raw_requests = cmd_args.r
 REQUESTS = [
-    {raw_requests[i]: {0: tonumber(raw_requests[i+1] if i+1 < len(raw_requests) else None, default=10)}}
+    {raw_requests[i]: {
+        0: tonumber(
+            raw_requests[i+1] if i+1 < len(raw_requests) else None,
+            default=10
+        ),
+        "pages": tonumber(
+            raw_requests[i+2] if (i+2 < len(raw_requests) and tonumber(
+                # safeguard to prevent unintentional utilization of parameters from another request
+                raw_requests[i+1] ,
+            )) else None,
+            default=1
+        ),
+    }}
     for i in range(0, len(raw_requests)) if tonumber(raw_requests[i], Not=True)
 ]
-PAGES = cmd_args.pages
 
 print(REQUESTS)
 # threads = os.listdir(FILE_PATH)
 # # print(threads[:10])
 
-# found = 0
 # for thread in threads:
 #     full_path = os.path.join(FILE_PATH, thread)
-#     if found != MAX_RESULTS and os.path.exists(os.path.join(full_path, "page_3.html")):
+
+#     if found != MAX_RESULTS and 
 #         found += 1
 #         print(full_path)
